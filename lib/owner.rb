241b92011ee0afc81f500c9b9679d1f27840686b
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-19 21:03:27 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,4 +42,36 @@ class Owner
   def buy_dog(name)
     @pets[:dogs] << Dog.new(name)
   end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
+
 end
