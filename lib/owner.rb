510f1eca4bdab8dbabdd62c0d2f3a6b7cbf03008
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-19 20:14:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    self.species
+  end
+
+  def name
+
+  end
+
 end
