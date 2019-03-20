e16f89ac91f30012db56c1a026a219858f673213
Henry Sztanski <henry@sztanski.com>
2019-03-20 19:22:39 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :pets
+  attr_accessor :pets, :name
   attr_reader :species
 
   @@all = []
@@ -22,6 +22,36 @@ class Owner
     self.pets[:fishes] << Fish.new(name)
   end
 
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each {|dog| dog.mood = "happy"} if !self.pets[:dogs].empty?
+  end
+
+  def play_with_cats
+    self.pets[:cats].each {|cat| cat.mood = "happy"} if !self.pets[:cats].empty?
+  end
+
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"} if !self.pets[:fishes].empty?
+  end
+
+  def sell_pets
+    self.pets.each do |type, pets|
+      pets.each {|pet| pet.mood = "nervous"}
+      pets.clear
+    end
+  end
+
+  def list_pets
+    list_hash = {}
+    self.pets.each {|type, pets| list_hash[type] = pets.count}
+    "I have #{list_hash[:fishes]} fish, #{list_hash[:dogs]} dog(s), and #{list_hash[:cats]} cat(s)."
+  end
+
+  # --- class methods ---
   def self.all
     @@all
   end
