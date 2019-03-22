9054721a221a7903c311c40b5cce622cdefd4e95
Eyal Toledano <eyaltoledano@me.com>
2019-03-22 20:21:09 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,6 +10,8 @@ class Owner
   def initialize(name)
     @name = name
     @@all << self
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def self.all
@@ -24,26 +26,60 @@ class Owner
     @@all.clear
   end
 
-  def species
-    @species = "human"
-  end
-
   def say_species
     "I am a #{species}."
   end
 
   def pets
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @pets
   end
 
   def buy_fish(name)
-    new_fish  = Fish.new(name)
-    new_fish.owner = self
-    @pets = @pets.each do |animal, animal_list|
-      if animal == :fishes
-        @pets[:fishes] << new_fish
-      end
+    fish = Fish.new(name)
+    fish.owner = self
+    fish.owner.pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    cat.owner = self
+    cat.owner.pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    dog.owner = self
+    dog.owner.pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
     end
-    pets = @pets
+  end
+
+  def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.pets.each do |pet_type, owned_pets|
+      owned_pets.each do |pet|
+        pet.mood = "nervous"
+      end
+      owned_pets.clear
+    end
+  end
+
+  def list_pets
+    "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
 end
