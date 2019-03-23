3a0aab5336909c774fb9c48be49d0602fcd59cc6
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-23 23:17:49 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
-  # code goes here
+attr_accessor :cat
+
+  describe "crookshanks"
+  let! (:crookshanks) { crookshanks.new }
+  it "can initialize a cat" do
+    post.cat = "crookshanks"
+   expect(cat).to be_a(Cat)
+  end
+
+it "cant change its name" do
+expect { cat.name = "mr. whiskers" }.to raise_error NoMethodError
+end
+
+it "initializes with nervous mood" do
+  expect(cat.mood).to eq("nervous")
+end
+
+it "can change its mood" do
+expect(cat.mood).to eq("playful")
+end
+
+
+end
 end
