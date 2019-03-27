ff1dbd3366dc2a8ecff158a4fca28ed2213d6601
Malcolm Leake <echelon1089@gmail.com>
2019-03-27 07:09:42 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
+  attr_accessor :pets 
+  
+  
+  def initialized
+    @pets = {fishes: [], cats: [], dogs: []}
+   end  
+   
+  def buy_cat(name)
+  cat = Cat.new(name)
+  end 
+  
+  def buy_dog(name)
+  dog = Dog.new(name)
+  end
+  
+  def buy_fish(name)
+  fish = Fish.new(name)
+  end
 end
