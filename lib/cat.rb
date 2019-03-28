6c6897b94b08e3b6bbacb3875db9cb4bd6bd9e5d
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-28 21:17:12 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+
+  attr_reader :name #can't change it's name cuz it can only read NOT write
+  attr_writer :mood #can change
+
+  def initialize(name)  #initializes new instance of a cat w/a name
+    @name = name
+    mood  #intializes with a "nervous" mood
+  end
+
+  def mood()
+  "nervous" #assigns mood (nervous || playful) to a new instance of Cat
+  end
+
 end
